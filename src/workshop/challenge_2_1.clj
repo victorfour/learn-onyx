8b2d420825fa349a682adc854e09377abfd53fe5
Victor FOUR <vfour@student.42.fr>
2019-01-22 09:58:43 +0100

    challenge_2_1

--- src/workshop/challenge_2_1.clj
+++ src/workshop/challenge_2_1.clj
@@ -24,6 +24,19 @@
 
       ;; <<< BEGIN FILL ME IN >>>
 
+      {:onyx/name :times-three
+       :onyx/fn :workshop.challenge-2-1/times-three
+       :onyx/type :function
+       :onyx/batch-size batch-size
+       :onyx/batch-timeout batch-timeout
+       :onyx/doc "Multiplies :n in the segment by 3"}
+
+      {:onyx/name :plus-fifty
+       :onyx/fn :workshop.challenge-2-1/plus-fifty
+       :onyx/type :function
+       :onyx/batch-size batch-size
+       :onyx/batch-timeout batch-timeout
+       :onyx/doc "Increments :n in the segment by 50"}
  
       ;; <<< END FILL ME IN >>>
 
