ff9395ff8341117c39c46850a0c71f4df4ec2520
Victor FOUR <vfour@student.42.fr>
2019-01-23 10:25:31 +0100

    challenge_4_1

--- src/workshop/challenge_4_1.clj
+++ src/workshop/challenge_4_1.clj
@@ -48,6 +48,11 @@
 
 ;; <<< BEGIN FILL ME IN FOR log-segments >>>
 
+(defn log-segments [event lifecycle]
+  (send logger (fn [_] (doseq [segment (:onyx.core/batch event)]
+                         (println segment))))
+  {})
+
 ;; <<< END FILL ME IN >>>
 
 (defn inject-writer-ch [event lifecycle]
@@ -58,10 +63,16 @@
 
 ;; <<< BEGIN FILL ME IN FOR logger-lifecycle calls >>>
 
+(def logger-lifecycle
+  {:lifecycle/after-batch log-segments})
+
 ;; <<< END FILL ME IN >>>
 
 (defn build-lifecycles []
   [;; <<< BEGIN FILL ME IN FOR :times-three >>>
+   {:lifecycle/task :times-three
+    :lifecycle/calls :workshop.challenge-4-1/logger-lifecycle
+    :onyx/doc "Logs messages about the task"}
 
    ;; <<< END FILL ME IN >>>
 
